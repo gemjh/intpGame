d4a8b1d289bc2f8fc13ff6a49a20c30b13352421
jhirn <74704541+gemjh@users.noreply.github.com>
2026-09-21 16:06:03 +0900

    Delete intp_android/intp_android directory

--- intp_android/intp_android/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// 루트 빌드 스크립트: 플러그인 버전만 선언하고, 실제 적용은 app 모듈에서 한다.
-plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-}
